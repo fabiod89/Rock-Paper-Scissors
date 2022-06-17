aff6cb49b0c9399689a45cd8d1e70bf9b1f1e049
Fabio D <fabio_d89@hotmail.com>
2022-06-16 22:51:21 -0400

    Add content

--- javascript.js
+++ javascript.js
@@ -3,22 +3,39 @@ let computerScore = 0;
 
 // Play a round
 function playRound (playerSelection,computerSelection){
+    document.getElementsByName("playerScore")[0].innerHTML = String(playerScore)
+    document.getElementsByName("computerScore")[0].innerHTML = String(computerScore)
+
+    document.getElementsByName("playerChoice")[0].src = (playerSelection + ".png");
+    document.getElementsByName("computerChoice")[0].src = (computerSelection + ".png");
     if (playerSelection === computerSelection){
-        alert("Tie");
+        document.getElementsByName("message")[0].innerHTML = "Tie!"
     }
     else if(playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
             playerSelection === "PAPER" && computerSelection === "ROCK" ||
             playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+
+        document.getElementsByName("message")[0].innerHTML = "You win!"
         playerScore++
-        alert("You win")
+        
     }
     else if(playerSelection === "ROCK" && computerSelection === "PAPER" ||
             playerSelection === "PAPER" && computerSelection === "SCISSORS" ||
             playerSelection === "SCISSORS" && computerSelection === "ROCK"){
+
+        document.getElementsByName("message")[0].innerHTML = "Computer wins!"
         computerScore++
-        alert("You lose")
+    }
+
+    if (computerScore === 5){
+        alert("Computer wins...")
+    }
+    else if (playerScore === 5){
+        alert("Player wins...")
     }
 }
+
+
 
 //Player choose
 document.getElementsByName("rock")[0].addEventListener("click", () => {playRound("ROCK",computerSelection())})
@@ -29,6 +46,7 @@ let computerSelection = () => {
     let choices = ["ROCK","SCISSORS","PAPER"]
     return (choices[Math.floor(Math.random() * choices.length)]);
 }
+
 
 
 
