d9f9bd7bc7d16598e1eca52a391eb6e415520c5a
Fabio D <fabio_d89@hotmail.com>
2022-06-17 10:48:01 -0400

    Restructure project files

--- javascript.js
+++ javascript.js
@@ -1,55 +1,10 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore = 0
 
-// Play a round
-function playRound (playerSelection,computerSelection){
-    document.getElementsByName("playerScore")[0].innerHTML = String(playerScore)
-    document.getElementsByName("computerScore")[0].innerHTML = String(computerScore)
+const rockButton = document.querySelector("#rock-selector")
+const paperButton = document.querySelector("#paper-selector")
+const scissorsButton = document.querySelector("#scissors-selector")
 
-    document.getElementsByName("playerChoice")[0].src = (playerSelection + ".png");
-    document.getElementsByName("computerChoice")[0].src = (computerSelection + ".png");
-    if (playerSelection === computerSelection){
-        document.getElementsByName("message")[0].innerHTML = "Tie!"
-    }
-    else if(playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
-            playerSelection === "PAPER" && computerSelection === "ROCK" ||
-            playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-
-        document.getElementsByName("message")[0].innerHTML = "You win!"
-        playerScore++
-        
-    }
-    else if(playerSelection === "ROCK" && computerSelection === "PAPER" ||
-            playerSelection === "PAPER" && computerSelection === "SCISSORS" ||
-            playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-
-        document.getElementsByName("message")[0].innerHTML = "Computer wins!"
-        computerScore++
-    }
-
-    if (computerScore === 5){
-        alert("Computer wins...")
-    }
-    else if (playerScore === 5){
-        alert("Player wins...")
-    }
-}
-
-
-
-//Player choose
-document.getElementsByName("rock")[0].addEventListener("click", () => {playRound("ROCK",computerSelection())})
-document.getElementsByName("paper")[0].addEventListener("click", () => {playRound("PAPER",computerSelection())})
-document.getElementsByName("scissors")[0].addEventListener("click", () => {playRound("SCISSORS",computerSelection())})
-//Computer choose
-let computerSelection = () => {
-    let choices = ["ROCK","SCISSORS","PAPER"]
-    return (choices[Math.floor(Math.random() * choices.length)]);
-}
-
-
-
-
-
-
-
+rockButton.addEventListener("click", () => {alert("hey")})
+paperButton.addEventListener("click", () => {alert("hey")})
+scissorsButton.addEventListener("click", () => {alert("hey")})
