457f96dcff18d4f16739c4b8d1db2f645dc1580d
Fabio D <fabio_d89@hotmail.com>
2022-06-18 16:25:46 -0400

    add player selection in JS

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,35 @@
+
+// Sprite variables
 let playerSprite = document.querySelector(".player-sprite")
 let playerSpriteIdle = "assets/player/player-idle.gif"
 let playerSpriteWalk = "assets/player/player-walk.gif"
 
+// Button assignment
 let rockButton = document.querySelector(".rock-button")
 let paperButton = document.querySelector(".paper-button")
 let scissorsButton = document.querySelector(".scissors-button")
 
-rockButton.querySelector("img").style.borderColor = "green"
-paperButton.querySelector("img").style.borderColor = "green"
-scissorsButton.querySelector("img").style.borderColor = "green"
+// Button listeners
+rockButton.addEventListener("click", () => {playerSelect(".rock-button")})
+paperButton.addEventListener("click", () => {playerSelect(".paper-button")})
+scissorsButton.addEventListener("click", () => {playerSelect(".scissors-button")})
 
+//Message
+
+let message = document.querySelector(".message")
+
+function buttonAnimation(choice){
+    document.querySelector(choice).querySelector("img").style.borderColor = "green"
+}
+
+
+function playerSelect(choice){
+    buttonAnimation(choice)
+    message.innerHTML = "Computer is choosing..."
+    setTimeout( () => {computerSelect()}, 2000);
+}
+
+function computerSelect(){
+    alert("heyyy")
+
+}
