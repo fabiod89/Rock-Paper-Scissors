c6f903b63048ec6f461428d18faa8c9b1f330076
Fabio D <fabio_d89@hotmail.com>
2022-06-18 16:02:12 -0400

    create variables

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,7 +2,11 @@ let playerSprite = document.querySelector(".player-sprite")
 let playerSpriteIdle = "assets/player/player-idle.gif"
 let playerSpriteWalk = "assets/player/player-walk.gif"
 
+let rockButton = document.querySelector(".rock-button")
+let paperButton = document.querySelector(".paper-button")
+let scissorsButton = document.querySelector(".scissors-button")
 
-let rockButton = document.querySelector(".rock-button").addEventListener("click", () => alert("test"))
-let paperButton = document.querySelector(".paper-button").addEventListener("click", () => alert("test"))
-let scissorsButton = document.querySelector(".scissors-button").addEventListener("click", () => alert("test"))
+rockButton.querySelector("img").style.borderColor = "green"
+paperButton.querySelector("img").style.borderColor = "green"
+scissorsButton.querySelector("img").style.borderColor = "green"
+
