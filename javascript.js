bb2d508810bff16ae84904f853eba8abc0c15aaf
Fabio D <fabio_d89@hotmail.com>
2022-06-17 22:51:52 -0400

    merge

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 
-const rockButton = document.querySelector("#rock-selector")
-const paperButton = document.querySelector("#paper-selector")
-const scissorsButton = document.querySelector("#scissors-selector")
+const rockButton = document.querySelector("#rock-selector");
+const paperButton = document.querySelector("#paper-selector");
+const scissorsButton = document.querySelector("#scissors-selector");
 
-rockButton.addEventListener("click", () => {alert("hey")})
-paperButton.addEventListener("click", () => {alert("hey")})
-scissorsButton.addEventListener("click", () => {alert("hey")})
+rockButton.addEventListener("click", () => {playRound("rock",computerChoice)});
+paperButton.addEventListener("click", () => {playRound("rock",computerChoice)});
+scissorsButton.addEventListener("click", () => {playRound("rock",computerChoice)});
+
+let computerChoice = () => {
+    return ("test");
+};
+
+function playRound(pChoice,cChoice){
+    alert(pChoice)
+    alert(cChoice)
+}
+
