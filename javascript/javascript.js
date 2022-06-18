b8c35e3d06498572ff6a18d906879f599b70d9f4
Fabio D <fabio_d89@hotmail.com>
2022-06-18 14:26:28 -0400

    add javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,8 @@
-.header{
+let playerSprite = document.querySelector(".player-sprite")
+let playerSpriteIdle = "assets/player/player-idle.gif"
+let playerSpriteWalk = "assets/player/player-walk.gif"
 
-}
+
+let rockButton = document.querySelector(".rock-button").addEventListener("click", () => alert("test"))
+let paperButton = document.querySelector(".paper-button").addEventListener("click", () => alert("test"))
+let scissorsButton = document.querySelector(".scissors-button").addEventListener("click", () => alert("test"))
