73df67947ac1a56d92fd6ee0fa1e5ca8e3fa7146
Fabio D <fabio_d89@hotmail.com>
2022-06-19 01:47:53 -0400

    add canvas to html

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,30 @@
 
+function draw() {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 50);
+      
+    };
+
+    img.src = "assets/player/player-idle.gif";
+  }
+
+  function draw2() {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const img = new Image();
+    ctx.drawImage(img, 250, 50);
+    img.onload = function() {
+      
+
+    };
+    img.src = "assets/player/player-idle.gif";
+  }
+
+
+draw()
+
+
 // Sprite variables
 let playerSprite = document.querySelector(".player-sprite")
 let playerSpriteIdle = "assets/player/player-idle.gif"
@@ -15,8 +41,13 @@ paperButton.addEventListener("click", () => {playerSelect(".paper-button")})
 scissorsButton.addEventListener("click", () => {playerSelect(".scissors-button")})
 
 //Message
-
 let message = document.querySelector(".message")
+
+function computerSelect(){
+    let choice = ".paper-button"
+    buttonAnimation(choice)
+
+}
 
 function buttonAnimation(choice){
     document.querySelector(choice).querySelector("img").style.borderColor = "green"
@@ -27,9 +58,4 @@ function playerSelect(choice){
     buttonAnimation(choice)
     message.innerHTML = "Computer is choosing..."
     setTimeout( () => {computerSelect()}, 2000);
-}
-
-function computerSelect(){
-    alert("heyyy")
-
 }
