71828901d66870a5a5fbe7155006e163a0576439
De Oliveira <fdeoliveira@msh.on.ca>
2022-06-20 18:02:13 -0400

    Finalize logic

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,7 +7,11 @@ let computerHealth = document.getElementById("enemy-health");
 playerHealth.innerHTML = 5;
 computerHealth.innerHTML = 5;
 
+let message = document.querySelector(".message");
+message.innerHTML = "Please choose an option..."
+
 let buttonsDisabled = false;
+let game = true
 
 function randomChoices() {
     const choices = [".enemy-rock-button",".enemy-paper-button",".enemy-scissors-button"];
@@ -18,18 +22,33 @@ function randomChoices() {
 function calculateRound(playerChoice,computerChoice){
     playerChoice = document.querySelector(playerChoice).name
     computerChoice = document.querySelector(computerChoice).name
-    if (playerChoice === computerChoice){
-        console.log("Tie")}
-    else if (playerChoice === "rock" && computerChoice === "scissors" ||
+    if (playerChoice === "rock" && computerChoice === "scissors" ||
         playerChoice === "scissors" && computerChoice === "paper" ||
         playerChoice === "paper" && computerChoice === "rock"){
-            playerHealth.innerHTML -= 1}
+            message.innerHTML = "Round won..."
+
+            setTimeout( () => { computerHealth.innerHTML -= 1 },2000)
+        }
     else if (computerChoice === "rock" && playerChoice === "scissors" ||
         computerChoice === "scissors" && playerChoice === "paper" ||
         computerChoice === "paper" && playerChoice === "rock"){
-            computerHealth.innerHTML -= 1}
-        
+            message.innerHTML = "Round lost..."
 
+            setTimeout( () => { playerHealth.innerHTML -= 1 },2000)
+        }
+    else{
+        message.innerHTML = "Tie!"
+    }
+}
+
+function checkWinner(){
+    if (playerHealth.innerHTML < 1){
+        game = false;
+        message.innerHTML = "You lose... Refresh to play again!"
+    }else if (computerHealth.innerHTML < 1){
+        game = false;
+        message.innerHTML = "You win... Refresh to play again!"
+    }
 }
 
 function resetButtons(playerChoice,computerChoice){
@@ -43,27 +62,34 @@ function buttonAnimation(playerChoice,computerChoice){
     document.querySelector(computerChoice).querySelector("img").style.borderColor = "red";
 }
 
-rockButton.addEventListener("click", () => {playRound(".rock-button")});
-paperButton.addEventListener("click", () => {playRound(".paper-button")});
-scissorsButton.addEventListener("click", () => {playRound(".scissors-button")});
+
+
+
 
 function playRound(playerChoice){
     buttonsDisabled = true;
-    if (buttonsDisabled){
+    if (buttonsDisabled && game){
         let computerChoice = randomChoices()
         buttonAnimation(playerChoice,computerChoice)
         calculateRound(playerChoice,computerChoice);
-        resetButtons(playerChoice,computerChoice)
-    
-        buttonsDisabled = false;
-    /* 
-    
-
-
-    resetButtons(pChoice,cChoice)
-    checkWinner();
-    buttonsDisabled = false;
-
-    */
+        
+        setTimeout( () => { resetButtons(playerChoice,computerChoice);buttonsDisabled = false;checkWinner() },2000)
+      
     }
 }
+
+rockButton.addEventListener("click", () => {if (buttonsDisabled === false){playRound(".rock-button")}});
+paperButton.addEventListener("click", () => {if (buttonsDisabled === false){playRound(".paper-button")}});
+scissorsButton.addEventListener("click", () => {if (buttonsDisabled === false){playRound(".scissors-button")}});
+
+
+
+
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let playerIdle = document.getElementById("playerIdle");
+let playerWalk = document.getElementById("playerWalk");
+let enemyIdle= document.getElementById("enemyIdle");
+
