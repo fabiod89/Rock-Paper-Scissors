805f56f96a180c8a63df3ed55bf9bf7b034c7c69
Fabio D <fabio_d89@hotmail.com>
2022-06-19 21:11:06 -0400

    add game logic

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,29 +1,6 @@
 
-function draw() {
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const img = new Image();
-    img.onload = function() {
-      ctx.drawImage(img, 0, 50);
-      
-    };
-
-    img.src = "assets/player/player-idle.gif";
-  }
-
-  function draw2() {
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const img = new Image();
-    ctx.drawImage(img, 250, 50);
-    img.onload = function() {
-      
-
-    };
-    img.src = "assets/player/player-idle.gif";
-  }
-
-
-draw()
-
+let playerHealth = 5
+let computerHealth = 5
 
 // Sprite variables
 let playerSprite = document.querySelector(".player-sprite")
@@ -35,6 +12,11 @@ let rockButton = document.querySelector(".rock-button")
 let paperButton = document.querySelector(".paper-button")
 let scissorsButton = document.querySelector(".scissors-button")
 
+//Enemy Button assignment
+let enemyRockButton = document.querySelector(".enemy-rock-button")
+let enemyPaperButton = document.querySelector(".enemy-paper-button")
+let enemyScissorsButton = document.querySelector(".enemy-scissors-button")
+
 // Button listeners
 rockButton.addEventListener("click", () => {playerSelect(".rock-button")})
 paperButton.addEventListener("click", () => {playerSelect(".paper-button")})
@@ -43,19 +25,68 @@ scissorsButton.addEventListener("click", () => {playerSelect(".scissors-button")
 //Message
 let message = document.querySelector(".message")
 
-function computerSelect(){
-    let choice = ".paper-button"
-    buttonAnimation(choice)
-
+let computerSelect = () => {
+    let choices = [".enemy-rock-button",".enemy-paper-button",".enemy-scissors-button"]
+    let choice = choices[Math.floor(Math.random()*choices.length)];
+    return (choice)
 }
 
 function buttonAnimation(choice){
     document.querySelector(choice).querySelector("img").style.borderColor = "green"
 }
 
+function enemyButtonAnimation(choice){
+    document.querySelector(choice).querySelector("img").style.borderColor = "red"
+}
 
-function playerSelect(choice){
-    buttonAnimation(choice)
+function calculateRound(playerChoice,computerChoice){
+
+    if (playerChoice === ".rock-button" && computerChoice === ".enemy-rock-button"){
+      return ("Tie")
+    }else if (playerChoice === ".rock-button" && computerChoice === ".enemy-scissors-button"){
+      return ("Player win")
+  }else if (playerChoice === ".rock-button" && computerChoice === ".enemy-paper-button"){
+    return ("Computer win")
+  }
+
+  else if (playerChoice === ".paper-button" && computerChoice === ".enemy-rock-button"){
+    return ("Player win")
+  }else if (playerChoice === ".paper-button" && computerChoice === ".enemy-scissors-button"){
+    return ("Computer win")
+}else if (playerChoice === ".paper-button" && computerChoice === ".enemy-paper-button"){
+  return ("Tie")
+}
+
+  else if (playerChoice === ".scissors-button" && computerChoice === ".enemy-rock-button"){
+    return ("Computer win")
+  }else if (playerChoice === ".scissors-button" && computerChoice === ".enemy-scissors-button"){
+    return ("Tie")
+  }else if (playerChoice === ".scissors-button" && computerChoice === ".enemy-paper-button"){
+    return ("Player win")
+  }
+
+}
+
+function calculateHealth(whoWon){
+  if (whoWon === "Tie"){
+    message.innerHTML = "Tie!!!"
+  }
+  else if(whoWon === "Player win"){
+    message.innerHTML = "Player attacks!"
+  }
+  else if(whoWon === "Computer win"){
+    message.innerHTML = "Computer attacks!"
+  }
+}
+
+function playerSelect(playerchoice){
+    buttonAnimation(playerchoice)
     message.innerHTML = "Computer is choosing..."
-    setTimeout( () => {computerSelect()}, 2000);
+    setTimeout( () => {
+      let computerChoice = computerSelect();
+      enemyButtonAnimation(computerChoice);
+      let whoWon = calculateRound(playerchoice,computerChoice);calculateHealth(whoWon);
+    }, 2000);
+
+
 }
